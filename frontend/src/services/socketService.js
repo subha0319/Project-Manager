38b0d242530ac43f7a1333949e4ed5ba3de2adae
subha0319 <subhashini0319@gmail.com>
2025-09-16 14:13:02 +0530

    resolve Socket error

--- frontend/src/services/socketService.js
+++ frontend/src/services/socketService.js
@@ -1,6 +1,10 @@
 import { io } from 'socket.io-client';
 
-// Connect to the backend server
-const socket = io('http://localhost:5001');
+//  Connect to the backend server
+// const socket = io('http://localhost:5001');
+
+// Use the environment variable for the live URL
+const URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:5001';
+const socket = io(URL);
 
 export default socket;
